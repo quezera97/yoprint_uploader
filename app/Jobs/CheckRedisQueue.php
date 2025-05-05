4362f162a168490211babf99eda8c4fd75ee6739
QueZera <zaherzulkifly@gmail.com>
2025-05-05 10:25:39 +0800

    Add redis, using predis

---
+++ app/Jobs/CheckRedisQueue.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CheckRedisQueue implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        // You can accept data here if needed
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::info('Redis is running, Meow!');
+    }
+}
--- app/Livewire/UploadFile/Table.php
+++ app/Livewire/UploadFile/Table.php
@@ -2,12 +2,28 @@
 
 namespace App\Livewire\UploadFile;
 
+use App\Jobs\CheckRedisQueue;
+use App\Models\UploadFile;
 use Livewire\Component;
 
 class Table extends Component
 {
+    public $count = 0;
+    public $uploadedFile = [];
+
+    public function mount()
+    {
+        CheckRedisQueue::dispatch();
+        $this->uploadedFile = $this->refreshUploadedFile();
+    }
+
     public function render()
     {
         return view('livewire.upload-file.table');
+    }
+
+    public function refreshUploadedFile()
+    {
+        $this->uploadedFile = UploadFile::get();
     }
 }
